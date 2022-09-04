8ed37b6d480ad8b7c748dea32a82f8cac8b6b611
sudomonikers <sudomonikers@gmail.com>
2022-09-04 11:33:30 -0400

    update tsconfig

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DropdownModule } from 'primeng/dropdown';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -20,7 +23,10 @@ import { QuerybuilderComponent } from './querybuilder/querybuilder.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    DropdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/querybuilder/querybuilder.component.ts
+++ src/app/querybuilder/querybuilder.component.ts
@@ -1,22 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { APIService } from '../API.service';
 import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-querybuilder',
   templateUrl: './querybuilder.component.html',
   styleUrls: ['./querybuilder.component.scss']
 })
-export class QuerybuilderComponent implements OnInit {
+export class QuerybuilderComponent implements OnInit, OnDestroy {
   queryBuilder: UntypedFormGroup;
+  tags: string[];
+  selectedTag: string = '';
+  subscription: Subscription;
 
   constructor(private api: APIService, private formBuilder: UntypedFormBuilder) {
+    this.tags = [
+      'HSK 1',
+      'HSK 2',
+      'HSK 3',
+      'HSK 4',
+      'HSK 5',
+      'HSK 6',
+      'HSK 7'
+    ]
     this.queryBuilder = this.formBuilder.group({
       tags: ['']
+    });
+    this.subscription = this.queryBuilder.valueChanges.subscribe((change) => {
+      console.log(change)
     })
    }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   query(): void {
